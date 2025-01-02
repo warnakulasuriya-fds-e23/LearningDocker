0ce64873cf13b335a312ff814f4e52f5b855f058
warnakulasuriya-fds-e23 <warnakulasuriya_fds_e23@engug.ruh.ac.lk>
2025-01-02 17:10:49 +0530

    now nodeapp container connects to mongo container for prod mode
    there is a error with a missing node package, nodemon package is not installed inside nodeapp container, need to fix this. probably an error with the if condition in 5th line in Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@ FROM node:20
 WORKDIR /app
 COPY package.json .
 ARG NODE_ENV
-RUN if [ "${NODE_ENV}" = "development" ]; \
+RUN if [ "$NODE_ENV" = "development" ]; \
         then npm install; \
         else npm install --only=production; \
         fi
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
 
@@ -7,5 +8,9 @@ const port = process.env.PORT || 3000;
 app.get("/", (req, res) => {
   res.send("<h2>Hello there:) Nice to meet you!!:)yup</h2>");
 });
+
+mongoose
+  .connect("mongodb://testuser:password123@172.18.0.2/?authSource=admin")
+  .then(() => console.log("Connected!"));
 
 app.listen(port, () => console.log(`listening on port : ${port}`));
